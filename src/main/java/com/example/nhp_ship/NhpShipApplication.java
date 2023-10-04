334306d2362014556a859ba87a876030f0d1c24b
QUANGG\QUANG <thanhnhan54>
2023-10-04 14:00:43 +0700

    first commit

---
+++ src/main/java/com/example/nhp_ship/NhpShipApplication.java
@@ -0,0 +1,13 @@
+package com.example.nhp_ship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhpShipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NhpShipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nhp_ship/NhpShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nhp_ship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhpShipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
